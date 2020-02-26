bafb9fe8a6bd116a323e80044295069b9674d42f
Dr. Jochen Manns <manns@insynergie.de>
2020-02-26 12:52:42 +0100

    using simplified collection factory

--- src/database/book.ts
+++ src/database/book.ts
@@ -1,14 +1,12 @@
-import { TGqlLayoutType } from '@jms-1/mongodb-graphql'
 import { Collection } from '@jms-1/mongodb-graphql/lib/collection'
 
 import { MongoConnection } from './connection'
 
-import { IBook } from '../client'
 import { Book } from '../models/book'
 
 export const BookCollection = MongoConnection.createCollection(
     Book,
-    class BookCollection extends Collection<IBook, TGqlLayoutType<typeof Book>> {
+    class extends Collection<typeof Book> {
         readonly collectionName = 'books'
     }
 )
